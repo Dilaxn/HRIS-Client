5cb875a58fff5d66b926fb9a3a365f4686bb08a3
Dilaxn <dilaxn@gmail.com>
2021-05-31 17:08:27 +0530

    Project retrive updated with small error

--- src/context/TimeContext/ProjectContext.js
+++ src/context/TimeContext/ProjectContext.js
@@ -1,12 +1,34 @@
 import React, {useState} from "react";
 import axios from "axios";
 
-export {readAllCustomers};
+export {readAllCustomers,readAllProjects};
 
 function readAllCustomers() {
     return Promise.resolve().then(() => {
         const tokenString = localStorage.getItem('id_token');
         return  axios.get('/customers', {
+            headers: {
+                Authorization: `Bearer ${tokenString}`,
+            },
+
+        })
+            .then(response => {
+                // setUserData(response.data);
+                // response.data.dependents.date_of_birth
+                return(response.data.data);
+            })
+            .catch((err) => {
+                console.log('Unable access ...');
+            });
+
+    });
+}
+
+
+function readAllProjects() {
+    return Promise.resolve().then(() => {
+        const tokenString = localStorage.getItem('id_token');
+        return  axios.get('/projects', {
             headers: {
                 Authorization: `Bearer ${tokenString}`,
             },
--- src/pages/Time/Projectinfo/Projects.js
+++ src/pages/Time/Projectinfo/Projects.js
@@ -1,65 +1,136 @@
-import 'date-fns';
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import DateFnsUtils from '@date-io/date-fns';
-import {
-    MuiPickersUtilsProvider,
-    KeyboardTimePicker,
-    KeyboardDatePicker,
-} from '@material-ui/pickers';
-import TextareaAutosize from "@material-ui/core/TextareaAutosize";
-import Button from "@material-ui/core/Button";
+import React, {useEffect, useState} from "react";
+import {Button, Grid} from "@material-ui/core";
+import {makeStyles} from "@material-ui/styles";
+import MUIDataTable from "mui-datatables";
 import {Link} from 'react-router-dom'
+import {useHistory} from "react-router";
+
+// components
+import PageTitle from "../../../components/PageTitle";
+
+// data
+import mock from "../../dashboard/mock";
+import {getToken, loginUser, readAllUsers} from "../../../context/UserContext";
+import {readAllJobs} from "../../../context/JobContext";
+import axios from "axios";
+import {readAllCustomers, readAllProjects} from "../../../context/TimeContext/ProjectContext";
+
+
+const useStyles = makeStyles(theme => ({
+    tableOverflow: {
+        overflow: 'auto'
+    }
+}))
 
 export default function Projects() {
-    // The first commit of Material-UI
-    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+    let [proData, setProData] = useState([]);
 
-    const handleDateChange = (date) => {
-        setSelectedDate(date);
+    let history = useHistory()
+
+    useEffect(() => {
+        readAllProjects().then(r => setProData(r))
+    }, []);
+
+
+    const details = [];
+    if (proData) {
+
+
+
+
+        proData.map(r => {
+            let x=''
+            const data = [
+                r.projectName,
+                r.customerName.customerName,
+                r.projectAdmin.map(r=>r.join(" ,")),
+                r._id
+            ]
+            details.push(data);
+        });
+    }
+
+    const options = {
+        selectableRowsOnClick: false,
+        onRowsDelete: async (rowsDeleted, dataRows) => {
+            console.log(rowsDeleted)
+        },
+        onRowClick: async (rowData) => {
+            var answer = window.confirm("Delete the data");
+            if (answer) {
+                const tokenString = getToken()
+                let x = [rowData[3]]
+                let id = [x[0]]
+                console.log(JSON.stringify({id}))
+                return axios.delete('/projects', {
+                    headers: {
+                        'Authorization': `Bearer ${tokenString}`,
+                        'Content-Type': 'application/json',
+                    },
+                    data: JSON.stringify({id})
+                })
+                    .then(function (response) {
+                        readAllProjects().then(r => setProData(r))
+                    })
+            } else {
+                //some code
+            }
+        },
+
     };
 
-    return (
-        <div>
-            <div>
-                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                    <h1>Projects</h1>
-                    <Grid container>
-                        <text style={{marginRight: 30}}>Customer Name</text>
-                        <TextareaAutosize aria-label="empty textarea" placeholder="Type for hint... "/>
-                    </Grid> <br/><br/>
-                    <Grid container>
-                        <text style={{marginRight: 30}}>Project</text>
-                        <TextareaAutosize aria-label="empty textarea" placeholder="Type for hint..."/>
-                    </Grid>
-                    <br/><br/><Grid container>
-                    <text style={{marginRight: 30}}>Project Admin</text>
-                    <TextareaAutosize aria-label="empty textarea" placeholder="Type for hint..."/>
-                </Grid>
-                    <br/><br/>
-                </MuiPickersUtilsProvider> <br/><br/>
-                <Grid container>
-                    <text style={{marginRight: 30}}>* Required field</text>
-                </Grid> <br/>
-                <Button variant="contained" color="primary" style={{margin: 20}}>
-                    View
-                </Button>
-                <Button variant="contained" style={{margin: 20}}>
-                    Cancle
-                </Button>
 
-            </div>
-            <hr/>
-            <div>
-                <Link to="/app/time/projectInfo/addProject">
-            <Button variant="contained" color="primary" style={{margin: 20}}>
-                Add
-            </Button>
-                </Link>
-            <Button variant="contained" style={{margin: 20}} color="secondary">
-                Delete
-            </Button>
-            </div>
-        </div>
+    const columns = [
+        {
+            name: "Project Name",
+            options: {
+                display: true,
+            }
+        },
+        {
+            name: "Customer Name",
+            options: {
+                display: true,
+            }
+        },
+        {
+            name: "Project Admins",
+            options: {
+                display: true,
+            }
+        },
+        {
+            name: "",
+            options: {
+                display: false,
+                onRowClick: (rowData, rowState) => {
+                    console.log(rowData, rowState);
+                },
+            }
+        },
+    ];
+
+
+    const classes = useStyles();
+    return (
+        <>
+            <PageTitle title="Add Project"/>
+            <Link to="/app/time/projectInfo/addProject">
+                <Button  variant="contained" color="primary" style={{margin: 20}} type="button">
+                    Add Project
+                </Button>
+            </Link>
+            <Grid container spacing={4}>
+                <Grid item xs={12}>
+                    <MUIDataTable
+                        title="Projects"
+                        data={details}
+                        columns={columns}
+                        options={options}
+                    />
+                </Grid>
+
+            </Grid>
+        </>
     );
 }
